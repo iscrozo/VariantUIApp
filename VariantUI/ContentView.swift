3c6bb5650ac9bb78e2ae464078d7f1fbf4492640
“Sebastian <iscrozo@gmail.com>
2023-11-06 22:05:17 -0500

    Initial Commit

---
+++ VariantUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VariantUI
+//
+//  Created by Camilo Rozo on 6/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VariantUI/VariantUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariantUIApp.swift
+//  VariantUI
+//
+//  Created by Camilo Rozo on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VariantUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
